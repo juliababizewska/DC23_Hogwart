25b599f2dea0ef1c5f6cd988a3d025282741b89c
sigmor10 <sigmor10@wp.pl>
2025-11-03 17:48:20 +0100

    Added email sending functionality

--- src/main/java/pl/hogwart/cvprocessor/services/CloudService.java
+++ src/main/java/pl/hogwart/cvprocessor/services/CloudService.java
@@ -9,6 +9,7 @@ import jakarta.mail.*;
 import jakarta.mail.internet.*;
 
 import java.io.File;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,13 +30,23 @@ public class CloudService {
 
     //Credentials to google account
     @Value("${google.account.app-password}")
-    private static String password;
+    private String password;
 
     @Value("${google.account.mail}")
-    private static String accountMail;
+    private String accountMail;
+
+    public CloudService(){
+        try{
+            Files.createDirectories(Paths.get(saveDir));
+        }
+        catch(Exception e){
+            System.out.println("Critical ERROR: Couldn't create download directory!!!");
+            System.out.println(e.getMessage());
+        }
+    }
 
     // Method establishes connection to the email server
-    private static Store establishConnection() throws MessagingException {
+    private Store establishImapConnection() throws MessagingException {
         Properties props = System.getProperties();
         props.setProperty("mail.store.protocol", "imaps");
         props.put("mail.imaps.ssl.enabled", "true");
@@ -44,6 +55,38 @@ public class CloudService {
         Store store = session.getStore("imaps");
         store.connect("imap.googlemail.com", accountMail, password);
         return store;
+    }
+
+    // Method establishes SMTP connection for sending out emails
+    private Session establishSmtpConnection() throws MessagingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.googlemail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(accountMail, password);
+            }
+        });
+        return session;
+    }
+
+    private String createResponseText(boolean accepted){
+        String text = "Szanowna/y Pani/e, \n\n";
+
+        if(accepted) {
+            text = text + "Gratulujemy przejścia do następnego etapu rekrutacji.";
+        }
+        else{
+            text = text + "Z przykrością informujemy że odrzuciliśmy twoją kandydaturę na aplikowane "
+                    + "stanowisko. Życzymy powodzenia w dalszych staraniach o zabezpieczenie zatrudnienia.";
+        }
+
+        text = text + "\n\n Pozdrawiam, \n Automatyczny system rozpatrzeń CV HogwartCVProcessor";
+        return text;
     }
 
     // Method downloads attachments to specified folder
@@ -60,8 +103,10 @@ public class CloudService {
                         String fileName = part.getFileName();
 
                         Path savePath = Paths.get(saveDir + File.separator + fileName);
-                        Files.copy(part.getInputStream(), savePath, StandardCopyOption.REPLACE_EXISTING);
-                        return savePath.toString();
+                        try(InputStream ins = part.getInputStream()) {
+                            Files.copy(ins, savePath, StandardCopyOption.REPLACE_EXISTING);
+                            return savePath.toString();
+                        }
                     }
                 }
             }
@@ -78,7 +123,7 @@ public class CloudService {
     public List<Applicant> getCVs() {
         List<Applicant> applicants = new ArrayList<>();
 
-        try (Store store = establishConnection()) {
+        try (Store store = establishImapConnection()) {
             Folder inbox = store.getFolder("inbox");
             inbox.open(Folder.READ_WRITE);
             Message[] mails = inbox.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
@@ -101,7 +146,7 @@ public class CloudService {
             return applicants;
         }
         catch (MessagingException e){
-            System.out.println("Error: Couldn't connect to mail server.");
+            System.out.println("Error: Couldn't connect to mail server via IMAP.");
             System.out.println(e.getMessage());
         }
         return null;
@@ -109,7 +154,21 @@ public class CloudService {
 
     // Method sends a response email to given applicant
     public void sendResponse(Applicant applicant, boolean accepted) {
+        try {
+            Session session = establishSmtpConnection();
+            Message message = new MimeMessage(session);
 
+            message.setFrom(new InternetAddress(accountMail));
+            message.setSubject("Hogwart Rekrutacja - Wynik pierwszego etapu.");
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(applicant.getEmail()));
+
+            message.setText(createResponseText(accepted));
+            Transport.send(message);
+        }
+        catch (MessagingException e) {
+            System.out.println("Error: Couldn't connect to mail server via SMTP.");
+            System.out.println(e.getMessage());
+        }
     }
 
     //Method sends file under given path to google drive cloud storage
