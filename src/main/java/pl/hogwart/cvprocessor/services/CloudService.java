8ebc99a6e90bf89b96b3688a6dfcc764ae4f1529
ShineExe <37691760+ShineExe@users.noreply.github.com>
2025-11-19 21:39:05 +0100

    Changed CloudService output location and re-added already processed data files to tracking

--- src/main/java/pl/hogwart/cvprocessor/services/CloudService.java
+++ src/main/java/pl/hogwart/cvprocessor/services/CloudService.java
@@ -32,8 +32,7 @@ import java.util.regex.Pattern;
 @Service
 public class CloudService {
     private final String subjectRegex = "Hogwart Rekrutacja.*";
-    private final String saveDir = "src/main/resources/cvs";
-    // TODO: the python extractor expects the cv files in data/cv_files folder
+    private final String saveDir = "data/cv_files";
 
     //Credentials to google account
     private final String password;
